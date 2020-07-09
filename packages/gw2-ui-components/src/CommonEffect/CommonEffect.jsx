a672c063177421d6c4128c1dda05f22961c6fa84
Manuel Haag <manuel@haag.online>
2020-07-09 10:59:14 +0200

    feat: add rigorous certainty effect

--- packages/gw2-ui-components/src/CommonEffect/CommonEffect.jsx
+++ packages/gw2-ui-components/src/CommonEffect/CommonEffect.jsx
@@ -5,15 +5,20 @@ import Effect from '../Effect';
 
 import commonEffects from '../data/commonEffects.json';
 
-const CommonEffect = forwardRef(({ name, ...rest }, ref) => (
-  <Effect
-    type="Common"
-    name={name}
-    description={commonEffects[name]}
-    {...rest}
-    ref={ref}
-  />
-));
+const CommonEffect = forwardRef(({ name: propsName, ...rest }, ref) => {
+  const name =
+    propsName === 'Mistlock Singularity' ? 'Rigorous Certainty' : propsName;
+
+  return (
+    <Effect
+      type="Common"
+      name={name}
+      description={commonEffects[name]}
+      {...rest}
+      ref={ref}
+    />
+  );
+});
 
 CommonEffect.propTypes = {
   name: PropTypes.oneOf(Object.keys(commonEffects)).isRequired,
