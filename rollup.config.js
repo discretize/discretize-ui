f426d934d257329f1658df3ebe73c68f12ca32d8
Huijie Shi <454906468@qq.com>
2021-11-21 21:47:38 +0800

    test

--- rollup.config.js
+++ rollup.config.js
@@ -1,34 +1,24 @@
 import babel from '@rollup/plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
+import externals from 'rollup-plugin-node-externals';
 
-export default {
-  input: 'src/index.js',
-  output: {
-    file: 'dist/bundle.js',
-    format: 'es',
-    sourcemap: true,
-    name: 'discretizeComponents',
+export default [
+  {
+    input: 'src/index.js',
+    output: [
+      { file: 'es/index.js', format: 'es' },
+      { file: 'lib/index.js', format: 'cjs' },
+    ],
+    plugins: [
+      externals({ deps: true }),
+      resolve({
+        extensions: ['.mjs', '.js', '.json', '.node', '.jsx'],
+      }),
+      babel({
+        presets: ['@babel/preset-env', ['@babel/preset-react', { 'runtime': 'automatic' }]],
+        babelHelpers: 'runtime',
+        plugins: [['@babel/plugin-transform-runtime', { 'corejs': 3 }]],
+      }),
+    ],
   },
-  external: [
-    /@material-ui\/core/,
-    /gw2-ui-bulk/,
-    /gatsby-plugin-image/,
-    /gw2-ui-components-bulk/,
-    /classnames/,
-    'react',
-    /react\//,
-    /@babel\/runtime/,
-  ],
-  plugins: [
-    resolve({
-      extensions: ['.mjs', '.js', '.json', '.node', '.jsx'],
-    }),
-    babel({
-      presets: ['@babel/preset-env', ['@babel/preset-react', { 'runtime': 'automatic' }]],
-      babelHelpers: 'runtime',
-      plugins: ['@babel/plugin-transform-runtime'],
-    }),
-    commonjs(),
-  ],
-};
+];
