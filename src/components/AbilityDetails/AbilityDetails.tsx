a6981bafdcaa3b1ca34b0d0e664bc21d3328f5a5
gw2princeps <21026467+gw2princeps@users.noreply.github.com>
2022-02-13 23:03:03 +0100

    feat: migrate AbilityDetails

---
+++ src/components/AbilityDetails/AbilityDetails.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { GW2ApiSkill } from '../../gw2api/types/skills/skill';
+import factsOrder from '../../helpers/factsOrder';
+import DetailsFact from '../DetailsFact/DetailsFact';
+import DetailsHeader from '../DetailsHeader/DetailsHeader';
+import DetailsText from '../DetailsText/DetailsText';
+import Spinner from '../Spinner/Spinner';
+import css from './AbilityDetails.module.css';
+
+export interface AbilityDetailsProps {
+  data: GW2ApiSkill;
+}
+
+// type is "skills" or "traits"
+const AbilityDetails = ({ data }: AbilityDetailsProps) => {
+  const { name, description: unparsedDescription, categories, facts } = data;
+
+  const { value: rechargeValue, icon: rechargeIcon } =
+    (facts && facts.find(({ type }) => type === 'Recharge')) || {};
+
+  let description = unparsedDescription;
+  if (categories && description) {
+    description = description.replace(
+      new RegExp(
+        `^(${categories.map((category) => `${category}\\. `).join('|')})`,
+        'g',
+      ),
+      '',
+    );
+  }
+
+  return (
+    <div>
+      <DetailsHeader
+        className={css.mb8}
+        {...(rechargeValue && {
+          flags: [
+            {
+              value: rechargeValue,
+              icon: rechargeIcon,
+            },
+          ],
+        })}
+      >
+        {name}
+      </DetailsHeader>
+      {!(unparsedDescription || categories || facts) && <Spinner />}
+      <DetailsText
+        lines={[
+          categories && (
+            <span className={css.detailsText}>
+              {categories.map((category) => `${category}. `)}
+            </span>
+          ),
+          description,
+        ]}
+        lineComponent="span"
+      />
+
+      {facts && (
+        <DetailsFact
+          facts={facts
+            .filter((fact) => fact.type !== 'Recharge')
+            .sort((a, b) => factsOrder[a.type] - factsOrder[b.type])}
+        />
+      )}
+    </div>
+  );
+};
+
+export default AbilityDetails;
