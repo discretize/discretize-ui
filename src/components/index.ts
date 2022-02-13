57eba428df4d2b5cac7db1b1f7c90f1b6c4281e4
Kulinda <39636810+kulinda@users.noreply.github.com>
2022-02-13 16:21:28 +0100

    fix: add Icon to the package export

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,6 @@ import Spinner from './Spinner/Spinner';
 import Tooltip from './Tooltip/Tooltip';
 import TooltipContainer from './TooltipContainer/TooltipContainer';
 import Progress from './Progress/Progress';
+import Icon from './Icon/Icon';
 
-export { Spinner, Tooltip, TooltipContainer, Progress };
+export { Spinner, Tooltip, TooltipContainer, Progress, Icon };
