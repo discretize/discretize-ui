2239373e0a004cd4c15b94da01b9076b6e8bc6c5
gw2princeps <21026467+gw2princeps@users.noreply.github.com>
2022-02-16 17:59:52 +0100

    chore: clean up remove, remove dead deps

--- .storybook/main.js
+++ .storybook/main.js
@@ -21,47 +21,7 @@ module.exports = {
       ],
       include: path.resolve(__dirname, '../src'),
     });
-    // To compile code written in the old monorepo structure without adjusting all the imports, we add some webpack aliases here
-    let alias = { ...(config.resolve.alias || {}) };
-    alias['gw2-ui-bulk'] = path.resolve(
-      path.join(__dirname, '..', 'packages', 'gw2-ui', 'src', 'index.js'),
-    );
-    alias['gw2-ui-builder-bulk'] = path.resolve(
-      path.join(
-        __dirname,
-        '..',
-        'packages',
-        'gw2-ui-builder',
-        'src',
-        'index.ts',
-      ),
-    );
-    alias['gw2-ui-components-bulk'] = path.resolve(
-      path.join(
-        __dirname,
-        '..',
-        'packages',
-        'gw2-ui-components',
-        'src',
-        'index.ts',
-      ),
-    );
-    alias['gw2-ui-redux-bulk'] = path.resolve(
-      path.join(__dirname, '..', 'packages', 'gw2-ui-redux', 'src', 'index.ts'),
-    );
-    config.resolve.alias = alias;
+
     return config;
-  },
-  babel: async (options) => {
-    // Hack the Babel config to use theme-ui
-    for (let p of options.presets) {
-      if (p instanceof Array) {
-        if (p[0].match(/\/@babel\/preset-react\//)) {
-          console.log(p[0], p[1]);
-          p[1].importSource = 'theme-ui';
-        }
-      }
-    }
-    return options;
   },
 };
