2a8495f60c32b62bcf7b1b4e57ef38c32220af4f
Kulinda <39636810+kulinda@users.noreply.github.com>
2022-02-16 19:04:58 +0100

    feat: use ResizeObserver instead of the react-resize-aware package

--- src/components/TraitLine/TraitLineConnector.tsx
+++ src/components/TraitLine/TraitLineConnector.tsx
@@ -1,6 +1,5 @@
 import clsx from 'clsx';
-import { ReactElement, useMemo, CSSProperties } from 'react';
-import useResizeAware from 'react-resize-aware';
+import { ReactElement, useMemo, CSSProperties, useRef, useEffect } from 'react';
 import css from './TraitlLineConnector.module.css';
 
 type Direction = 'up' | 'mid' | 'down';
@@ -19,73 +18,85 @@ const TraitLineConnector = ({
   disabled,
   className,
 }: TraitLineConnectorProps): ReactElement => {
-  const [resizeListener, { width, height }] = useResizeAware();
-  const style: CSSProperties | undefined = useMemo(() => {
-    if (width === null || height === null) return;
-    let startY;
-    switch (start) {
-      case 'up':
-        startY = height - 18.5;
-        break;
-      case 'mid':
-        startY = height * 0.5;
+  let ref_container = useRef<HTMLDivElement>(null);
+  let ref_line = useRef<HTMLDivElement>(null);
 
-        if (end === 'up') {
-          startY += 4;
-        } else if (end === 'down') {
-          startY -= 4;
-        }
+  useEffect(() => {
+    if (!ref_container.current) return;
+    let redraw = () => {
+      if (!ref_container.current || !ref_line.current) return;
 
-        break;
-      case 'down':
-        startY = 18.5;
-        break;
-      default:
-        return;
-    }
+      let { width, height } = ref_container.current.getBoundingClientRect();
+      let startY: number;
+      switch (start) {
+        case 'up':
+          startY = height - 18.5;
+          break;
+        case 'mid':
+          startY = height * 0.5;
 
-    let endY;
-    switch (end) {
-      case 'up':
-        endY = height - 18.5;
-        break;
-      case 'mid':
-        endY = height * 0.5;
+          if (end === 'up') {
+            startY += 4;
+          } else if (end === 'down') {
+            startY -= 4;
+          }
 
-        if (start === 'up') {
-          endY += 4;
-        } else if (start === 'down') {
-          endY -= 4;
-        }
+          break;
+        case 'down':
+          startY = 18.5;
+          break;
+        default:
+          return;
+      }
 
-        break;
-      case 'down':
-        endY = 18.5;
-        break;
-      default:
-        return;
-    }
+      let endY: number;
+      switch (end) {
+        case 'up':
+          endY = height - 18.5;
+          break;
+        case 'mid':
+          endY = height * 0.5;
 
-    const length = Math.sqrt(width ** 2 + (startY - endY) ** 2);
-    const angle = Math.atan2(endY - startY, -width) * (180 / Math.PI);
+          if (start === 'up') {
+            endY += 4;
+          } else if (start === 'down') {
+            endY -= 4;
+          }
 
-    return {
-      bottom: (startY + endY) / 2 - 8 / 2 + 'px',
-      left: width / 2 - length / 2 + 'px',
-      width: length + 'px',
-      transform: `rotate(${angle}deg)`,
+          break;
+        case 'down':
+          endY = 18.5;
+          break;
+        default:
+          return;
+      }
+
+      const length = Math.sqrt(width ** 2 + (startY - endY) ** 2);
+      const angle = Math.atan2(endY - startY, -width) * (180 / Math.PI);
+
+      const style = ref_line.current.style;
+      style.bottom = (startY + endY) / 2 - 8 / 2 + 'px';
+      style.left = width / 2 - length / 2 + 'px';
+      style.width = length + 'px';
+      style.transform = `rotate(${angle}deg)`;
     };
-  }, [start, end, width, height]);
+
+    const obs = new ResizeObserver(redraw);
+    obs.observe(ref_container.current);
+    return () => {
+      obs.disconnect();
+    };
+  }, [start, end, disabled]);
 
   return (
-    <div className={clsx(className, css.root)}>
-      {resizeListener}
-
-      {style && !disabled ? (
-        <div style={style} className={css.wrapper}>
-          <div className={css.traitlineConnector} />
-        </div>
-      ) : null}
+    <div className={clsx(className, css.root)} ref={ref_container}>
+      <div
+        style={{ visibility: disabled ? 'hidden' : 'visible' }}
+        className={css.wrapper}
+        ref={ref_line}
+      >
+        <div className={css.traitlineConnector} />
+      </div>
     </div>
   );
 };
