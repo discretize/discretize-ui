140be83bf1da37c58674d94ee27b81b000af7562
gw2princeps <21026467+gw2princeps@users.noreply.github.com>
2022-02-18 13:06:28 +0100

    fix: traited fact ts error

--- src/gw2api/types/common/traited_fact.ts
+++ src/gw2api/types/common/traited_fact.ts
@@ -1,8 +1,8 @@
 import GW2ApiFact from './fact';
 
-interface GW2ApiTraitedFact extends GW2ApiFact {
+type GW2ApiTraitedFact = GW2ApiFact & {
   requires_trait: number;
   overrides?: number;
-}
+};
 
 export default GW2ApiTraitedFact;
