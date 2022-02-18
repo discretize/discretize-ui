60e88b5348563ced8f5b7785353fce59c0822397
Kulinda <39636810+kulinda@users.noreply.github.com>
2022-02-18 16:59:40 +0100

    fix: name the css import, otherwise ts will include it in its bindings

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
-import './global.module.css';
+// This import needs a name, otherwise TypeScript will mention it in its bindings.
+import _css from './global.module.css';
 
 export * from './components/index';
