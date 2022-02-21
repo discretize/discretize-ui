ea0f4e912dab44a6fff9036f250fc2568e0d073e
gw2princeps <21026467+gw2princeps@users.noreply.github.com>
2022-02-21 20:46:09 +0100

    feat: export language provider

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
+import { APILanguageProvider } from './gw2api/hooks';
+import { capitalize } from './helpers/capitalize';
+
+export { APILanguageProvider, capitalize };
 export * from './components/index';
