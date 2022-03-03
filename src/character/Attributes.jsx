ff0d6c4edb9aa81889bba72ac7ab527ac77c7a27
gw2princeps <21026467+gw2princeps@users.noreply.github.com>
2022-03-03 11:08:38 +0100

    fix: proper padding for attributes

--- src/character/Attributes.jsx
+++ src/character/Attributes.jsx
@@ -12,6 +12,9 @@ const useStyles = makeStyles()(() => ({
     color: '#AAAAAA',
     whiteSpace: 'nowrap',
   },
+  gridItem: {
+    padding: '4px 8px',
+  },
 }));
 
 const Attributes = ({ profession, data }) => {
@@ -21,39 +24,39 @@ const Attributes = ({ profession, data }) => {
       <Grid container>
         <Grid item xs={6}>
           <List dense disablePadding>
-            <ListItem>
+            <ListItem className={classes.gridItem}>
               <Attribute name="Power" text={`${data.Power}`} className={classes.gw2Item} />
             </ListItem>
-            <ListItem>
+            <ListItem className={classes.gridItem}>
               <Attribute name="Toughness" text={`${data.Toughness}`} className={classes.gw2Item} />
             </ListItem>
-            <ListItem>
+            <ListItem className={classes.gridItem}>
               <Attribute name="Vitality" text={`${data.Vitality}`} className={classes.gw2Item} />
             </ListItem>
-            <ListItem>
+            <ListItem className={classes.gridItem}>
               <Attribute name="Precision" text={`${data.Precision}`} className={classes.gw2Item} />
             </ListItem>
-            <ListItem>
+            <ListItem className={classes.gridItem}>
               <Attribute name="Ferocity" text={`${data.Ferocity}`} className={classes.gw2Item} />
             </ListItem>
-            <ListItem>
+            <ListItem className={classes.gridItem}>
               <Attribute
                 name="Condition Damage"
                 text={`${data['Condition Damage']}`}
                 className={classes.gw2Item}
               />
             </ListItem>
-            <ListItem>
+            <ListItem className={classes.gridItem}>
               <Attribute name="Expertise" text={`${data.Expertise}`} className={classes.gw2Item} />
             </ListItem>
-            <ListItem>
+            <ListItem className={classes.gridItem}>
               <Attribute
                 name="Concentration"
                 text={`${data.Concentration}`}
                 className={classes.gw2Item}
               />
             </ListItem>
-            <ListItem>
+            <ListItem className={classes.gridItem}>
               <Attribute
                 name="Agony Resistance"
                 text={`${data['Agony Resistance']}`}
@@ -64,55 +67,51 @@ const Attributes = ({ profession, data }) => {
         </Grid>
         <Grid item xs={6}>
           <List dense disablePadding>
-            <ListItem>
-              <Profession
-                name={firstUppercase(profession)}
-                text={'0'}
-                className={classes.gw2Item}
-              />
+            <ListItem className={classes.gridItem}>
+              <Profession name={firstUppercase(profession)} text="0" className={classes.gw2Item} />
             </ListItem>
-            <ListItem>
+            <ListItem className={classes.gridItem}>
               <Attribute name="Armor" text={`${data.Armor}`} className={classes.gw2Item} />
             </ListItem>
-            <ListItem>
+            <ListItem className={classes.gridItem}>
               <Attribute name="Health" text={`${data.Health}`} className={classes.gw2Item} />
             </ListItem>
-            <ListItem>
+            <ListItem className={classes.gridItem}>
               <Attribute
                 name="Critical Chance"
                 text={`${Math.round(data['Critical Chance'] * 10000) / 100}%`}
                 className={classes.gw2Item}
               />
             </ListItem>
-            <ListItem>
+            <ListItem className={classes.gridItem}>
               <Attribute
                 name="Critical Damage"
                 text={`${Math.round(data['Critical Damage'] * 1000) / 10}%`}
                 className={classes.gw2Item}
               />
             </ListItem>
-            <ListItem>
+            <ListItem className={classes.gridItem}>
               <Attribute
                 name="Healing Power"
                 text={`${data['Healing Power']}`}
                 className={classes.gw2Item}
               />
             </ListItem>
-            <ListItem>
+            <ListItem className={classes.gridItem}>
               <Attribute
                 name="Condition Duration"
                 text={`${Math.round(data['Condition Duration'] * 10000) / 100}%`}
                 className={classes.gw2Item}
               />
             </ListItem>
-            <ListItem>
+            <ListItem className={classes.gridItem}>
               <Attribute
                 name="Boon Duration"
                 text={`${Math.round(data['Boon Duration'] * 10000) / 100}%`}
                 className={classes.gw2Item}
               />
             </ListItem>
-            <ListItem>
+            <ListItem className={classes.gridItem}>
               <Attribute name="Magic Find" text="0" className={classes.gw2Item} />
             </ListItem>
           </List>
