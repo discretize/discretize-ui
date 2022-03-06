c458e4dbef551dd0ced9ae026cb69d412dfa967d
gw2princeps <21026467+gw2princeps@users.noreply.github.com>
2022-03-07 00:03:30 +0100

    feat: ger translation for control effect

--- src/components/ControlEffect/ControlEffect.stories.tsx
+++ src/components/ControlEffect/ControlEffect.stories.tsx
@@ -1,6 +1,8 @@
 import type { ComponentMeta, ComponentStory } from '@storybook/react';
 import React from 'react';
 import ControlEffect from './ControlEffect';
+import { Multiple } from '../Item/Item.stories';
+import { APILanguageProvider } from '../../i18n';
 
 export default {
   title: 'Components/ControlEffect',
@@ -18,3 +20,18 @@ export const Daze = Template.bind({});
 Daze.args = {
   name: 'Daze',
 };
+
+export function Translated() {
+  return (
+    <>
+      This is an english effect: <ControlEffect name="Daze" />,{' '}
+      <ControlEffect name="Knockback" />
+      <APILanguageProvider value="de">
+        <p>
+          This is a chinese effect:
+          <ControlEffect name="Daze" />, <ControlEffect name="Knockback" />
+        </p>
+      </APILanguageProvider>
+    </>
+  );
+}
--- src/i18n/controlEffects.ts
+++ src/i18n/controlEffects.ts
@@ -2,34 +2,66 @@ import { Translation } from '.';
 import { ControlEffectTypes } from '../data/controlEffects';
 
 export const CONTROL_EFFECTS: Record<ControlEffectTypes, Translation> = {
-  Daze: {},
-  Float: {},
-  Knockback: {},
-  Knockdown: {},
-  Launch: {},
-  Pull: {},
-  Sink: {},
-  Stun: {},
+  Daze: {
+    de: 'Benommenheit',
+  },
+  Float: {
+    de: 'Float',
+  },
+  Knockback: {
+    de: 'Rückschlag',
+  },
+  Knockdown: {
+    de: 'Niederschlag',
+  },
+  Launch: {
+    de: 'Hochschleudern',
+  },
+  Pull: {
+    de: 'Ziehen',
+  },
+  Sink: {
+    de: 'Sinken',
+  },
+  Stun: {
+    de: 'Betäubung',
+  },
 };
 
 export const CONTROL_EFFECTS_DESCRIPTIONS: Record<
   ControlEffectTypes,
   Translation
 > = {
-  Daze: { en: 'Disables all skills for a short duration.' },
-  Float: { en: 'Causes the underwater target to move upwards.' },
+  Daze: {
+    en: 'Disables all skills for a short duration.',
+    de: 'Fertigkeiten können nicht aktiviert werden; Dauer summiert sich.',
+  },
+  Float: {
+    en: 'Causes the underwater target to move upwards.',
+    de: 'Schweben ist ein Unterwasser-Effekt, der das Ziel oder sich selbst in Richtung Wasseroberfläche bewegt.',
+  },
   Knockback: {
     en: 'Knocks back the target away and on the ground, preventing movement and actions for a short duration.',
+    de: 'Rückschlag ist ein Status-Effekt, der zu Folge hat, dass man vom Verursacher weggestoßen wird',
   },
   Knockdown: {
     en: 'Knocks the target on ground, preventing movement and actions for a short duration.',
+    de: 'Niederschlag wirft den Gegner zu Boden, wodurch er für kurze Zeit bewegungsunfähig ist und keine Fertigkeiten verwenden kann.',
   },
   Launch: {
     en: 'Throws the target in the air over a short distance, preventing movement and actions for a short duration. Can move Downed targets.',
+    de: 'Durch Hochschleudern wird das Ziel unterbrochen und in die Luft geworfen.',
   },
   Pull: {
     en: 'Pulls the caster to the target or the target to a specific location and disables them for a short duration.',
+    de: 'Ziehen unterbricht Fertigkeiten mit Wirkzeit. Ziehen reduziert die Trotzleiste von Kreaturen abhängig von der aufgerundeten angewendeten Dauer.',
   },
-  Sink: { en: 'Causes the underwater target to move downwards.' },
-  Stun: { en: 'Prevents movement and actions for a short duration.' },
+  Sink: {
+    en: 'Causes the underwater target to move downwards.',
+    de: 'Sinken verhindert Bewegung und Fertigkeitsaktivierung und lässt Betroffene sinken.',
+  },
+  Stun: {
+    en: 'Prevents movement and actions for a short duration.',
+    de: 'Handlungsunfähig; Dauer summiert sich.',
+  },
 };
