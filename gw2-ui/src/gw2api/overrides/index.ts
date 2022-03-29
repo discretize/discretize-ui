0f09ab4427db226c57a112138e8fb6d42d64fc12
Kulinda <39636810+kulinda@users.noreply.github.com>
2022-03-29 15:34:27 +0200

    feat: add override for skill 10586

--- gw2-ui/src/gw2api/overrides/index.ts
+++ gw2-ui/src/gw2api/overrides/index.ts
@@ -4,8 +4,12 @@ import type GW2ApiSpecialization from '../types/specialization/specialization';
 import type GW2ApiTrait from '../types/traits/trait';
 import type { Override } from '../cache';
 import { fixFactsInSkill, fixFactsInTrait } from './skill_facts';
+import { fixSkillTypes } from './skill_types';
 
-export const SKILL_OVERRIDES: Override<GW2ApiSkill>[] = [fixFactsInSkill];
+export const SKILL_OVERRIDES: Override<GW2ApiSkill>[] = [
+  fixFactsInSkill,
+  fixSkillTypes,
+];
 
 export const TRAIT_OVERRIDES: Override<GW2ApiTrait>[] = [fixFactsInTrait];
 
---
+++ gw2-ui/src/gw2api/overrides/skill_types.ts
@@ -0,0 +1,14 @@
+import GW2ApiSkill from '../types/skills/skill';
+
+export function fixSkillTypes(
+  id: number,
+  item: GW2ApiSkill | undefined,
+): GW2ApiSkill | undefined {
+  if (!item) return item;
+  // Compare:
+  // https://api.guildwars2.com/v2/skills?ids=10586
+  // https://api.guildwars2.com/v2/skills?ids=10586&lang=zh
+  if ((item.type as string) === 'Transform') item.type = 'Profession';
+  if ((item.slot as string) === 'Transform_1') item.slot = 'Profession_1';
+  return item;
+}
