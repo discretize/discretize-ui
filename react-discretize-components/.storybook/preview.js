9a0ff059ba5030bbe9c6f161d1bb4e99df549637
gw2princeps <21026467+gw2princeps@users.noreply.github.com>
2022-03-31 22:00:29 +0200

    feat(rdc): add theme decorator for storybook

--- react-discretize-components/.storybook/preview.js
+++ react-discretize-components/.storybook/preview.js
@@ -1,5 +1,12 @@
 import '@discretize/typeface-menomonia';
 import '../../gw2-ui/src/default_style.css';
+import 'typeface-fira-mono';
+import 'typeface-muli';
+import 'typeface-raleway';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import muiTheme from '../src/styles/muiTheme';
+import globals from '../src/styles/globals';
+import { Global } from '@emotion/react';
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
@@ -10,3 +17,16 @@ export const parameters = {
     },
   },
 };
+
+// setup the MUI theming
+export const decorators = [
+  (Story) => (
+    <>
+      <Global globals={globals} />
+      <ThemeProvider theme={muiTheme}>
+        <CssBaseline />
+        <Story />
+      </ThemeProvider>
+    </>
+  ),
+];
