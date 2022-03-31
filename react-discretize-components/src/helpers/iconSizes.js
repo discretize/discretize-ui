59ed40fa27afe117545a4534e0cf1c5c7507e9ad
gw2princeps <21026467+gw2princeps@users.noreply.github.com>
2022-03-31 18:16:54 +0200

    feat(rdc): migrate iconsizes to ts

--- react-discretize-components/src/helpers/iconSizes.js
+++
@@ -1,9 +0,0 @@
-export default {
-  tiny: 12,
-  small: 16,
-  medium: 20,
-  medium2: 30,
-  large: 40,
-  large2: 45,
-  big: 60,
-};
---
+++ react-discretize-components/src/helpers/iconSizes.ts
@@ -0,0 +1,20 @@
+export type IconSizes =
+  | 'tiny'
+  | 'small'
+  | 'medium'
+  | 'medium2'
+  | 'large'
+  | 'large2'
+  | 'big';
+
+const sizes: Record<IconSizes, number> = {
+  tiny: 12,
+  small: 16,
+  medium: 20,
+  medium2: 30,
+  large: 40,
+  large2: 45,
+  big: 60,
+};
+
+export default sizes;
