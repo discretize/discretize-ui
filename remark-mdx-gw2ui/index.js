6072b90cc5359be04f2c81681aff1760594d9bad
gw2princeps <21026467+gw2princeps@users.noreply.github.com>
2022-11-18 11:25:15 +0100

    fix: double added id

--- remark-mdx-gw2ui/index.js
+++ remark-mdx-gw2ui/index.js
@@ -1,8 +1,6 @@
 import visit from 'unist-util-visit';
 import resolve from './map-gw2-ids.js';
 
-import { injectCharacterProps } from './injectCharacterProps.js';
-
 function attrToProps(attr) {
   const props = {};
   attr && attr.forEach((attr) => (props[attr.name] = attr.value));
@@ -23,26 +21,9 @@ export default () => {
         );
 
         node.attributes = [
-          ...node.attributes,
+          ...node.attributes.filter((attr) => attr.name !== 'id'),
           { type: 'mdxJsxAttribute', name: 'id', value: resolvedProps.id },
         ];
-      } else if (componentType === 'Character') {
-        const oldAttributeNode = node.attributes.find(
-          (attr) => attr.name === 'gear',
-        );
-        const gearRaw = oldAttributeNode?.value;
-        const gear = JSON.parse((gearRaw || '').replaceAll('\\', ''));
-        const injectedProps = injectCharacterProps(gear);
-
-        if (injectedProps) {
-          node.attributes = [
-            ...node.attributes.filter((attr) => attr.name !== 'gear'),
-            {
-              ...oldAttributeNode,
-              value: JSON.stringify(injectedProps),
-            },
-          ];
-        }
       }
     });
 
