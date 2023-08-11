be3e46d368c759e96cf2db0f7ddb090a3c116f24
Princeps <21026467+gw2princeps@users.noreply.github.com>
2023-08-11 15:03:45 +0200

    Update mistlockInstabilities.ts

--- gw2-ui/src/i18n/mistlockInstabilities.ts
+++ gw2-ui/src/i18n/mistlockInstabilities.ts
@@ -39,70 +39,70 @@ export const MISTLOCK_INSTABILITIES_DESCRIPTIONS: Record<
 > = {
   'Adrenaline Rush': {
     en: "When enraged, enemies deal 100% increased damage while they're low on health but can no longer critically hit. Enemies deal 10% less damage when not enraged. ",
-    de: '',
+    de: 'Wenn sie erzürnt sind, während Ihre Lebenspunkte niedrig sind, fügen Gegner 150 % erhöhten Schaden zu. Wenn sie nicht erzürnt sind, fügen Gegner 20 % weniger Schaden zu.',
   },
   'Afflicted': {
     en: 'Outgoing resistance and resolution duration is increased by 33%. Enemies apply random damaging conditions. ',
-    de: '',
+    de: 'Die Dauer gewährten Widerstands ist um 50 % erhöht. Gegner verursachen zufällige Zustände.',
   },
   'Boon Overload': {
     en: 'Each boon reduces maximum health by 1.5%. Outgoing boon duration is increased by 20%. ',
-    de: '',
+    de: 'Jeder Segen reduziert die maximalen Lebenspunkte um 1.5%. Die Dauer gewährter Segen wird um 20% erhöht.',
   },
   'Flux Bomb': {
     en: "You are periodically afflicted by the Anomaly's flux bombs. Flux bombs apply to both allies and enemies. Enemies standing in flux bombs are periodically afflicted with random conditions. ",
-    de: '',
+    de: 'Die Fluxbomben der Anomalie machen Euch in regelmäßigen Abständen zu schaffen. Fluxbomben werden sowohl bei Verbündeten, als auch bei Gegnern angewendet.',
   },
   'Fractal Vindicators': {
     en: 'Fractal Avengers are replaced by more-powerful Fractal Vindicators.',
-    de: '',
+    de: 'Fraktal-Rächer werden durch kraftvollere Fraktal-Verteidiger ersetzt.',
   },
   'Frailty': {
     en: 'Players are smaller and take 10% more damage, but move 25% faster. ',
-    de: '',
+    de: 'Spieler sind kleiner und haben 30 % weniger Lebenspunkte, bewegen sich jedoch 25 % schneller.',
   },
   'Hamstrung': {
     en: 'Low health increasingly slows your movements, up to 33%. Endurance regenerates 33% faster. ',
-    de: '',
+    de: 'Niedrige Lebenspunkte verlangsamen Eure Bewegungen zunehmend. Ausdauer regeneriert sich 33 % schneller.',
   },
   'Last Laugh': {
     en: 'Enemies explode upon dying if not stunned. Stunned enemies apply protection, regeneration, and stability to nearby allies. ',
-    de: '',
+    de: 'Gegner explodieren beim Tod, wenn sie nicht betäubt sind. Betäubte Gegner gewähren Verbündeten in der Nähe Schutz und Stabilität.',
   },
   'Mists Convergence': {
     en: 'The Fractals of the Mists are blurring together...',
-    de: '',
+    de: 'Die Fraktale der Nebel verschwimmen miteinander ...',
   },
   'No Pain, No Gain': {
     en: 'Enemies receive boons when hit. Stripping an enemy boon will apply that boon to nearby allies.<br/><c=@reminder>(Cooldown: 20 seconds)</c>',
-    de: '',
+    de: 'Gegner erhalten bei Treffern Segen. Die Entfernung eines gegnerischen Segens stiehlt ihnen Lebenspunkte. <br/><c=@reminder>(Erholzeit: 20 Sekunden)</c>',
   },
   'Outflanked': {
     en: 'Allies within a range of 300 take 100% damage when attacked from behind. Non-boss enemies within a range of 300 take 100% damage when attacked from behind or the side.',
-    de: '',
+    de: 'Verbündete in einem Umkreis von 300 erleiden 100% Schaden, wenn sie von hinten angegriffen werden. Nicht-Boss-Feinde innerhalb einer Reichweite von 300 erleiden 100 % Schaden, wenn sie von hinten oder von der Seite angegriffen werden.',
   },
   'Social Awkwardness': {
     en: 'Players will be pushed away from one another. Stability negates this effect. ',
-    de: '',
+    de: 'Spieler werden von einander weg gestoßen. Stabilität negiert diesen Effekt.',
   },
   'Stick Together': {
     en: 'Take 25% increased damage when not within 300 range of an ally. Deal 5% increased damage when within a range of 300 of an ally.',
-    de: '',
+    de: 'Erhöht den Schaden um 25%, wenn er sich nicht in 300er Reichweite eines Verbündeten befindet. Verursache 5% mehr Schaden, wenn du dich in einer Reichweite von 300 von einem Verbündeten befindest.',
   },
   'Sugar Rush': {
     en: "Increases movement speed and attack speed by 15%. Elite and lower enemies' movement speed and attack speed increases by 25%, but critical chance is reduced by 25%. ",
-    de: '',
+    de: 'Erhöht das Bewegungstempo und das Angriffstempo um 15%. Das Bewegungstempo und das Angriffstempo von Elite- und niederen Gegnern wird um 25% erhöht, aber die Chance auf kritische Treffer wird um 25% verringert.',
   },
   'Toxic Trail': {
     en: 'Enemies leave a path of poison behind them. Blocking a toxic trail attack will absorb it, cleanse poison, and pulse exposed to enemies near the source of the trail.',
-    de: '',
+    de: 'Gegner hinterlassen eine Giftspur. Beim Blocken eines "Toxische Spur"-Angriffs wird sie absorbiert und der Zustand beseitigt.',
   },
   'Vengeance': {
     en: 'When enemies die, they enhance nearby foes with multiple boons. Strip any enemy boon to inflict them with weakness and damage nearby foes.',
-    de: '',
+    de: 'Wenn Gegner sterben, verbessern sie Gegner in der Nähe mit mehreren Segen. Dies trifft bei Elite-Gegnern nicht zu. Entfernt jeden Segen von Gegnern, um ihnen Schwäche zuzufügen.',
   },
   'Toxic Sickness': {
     en: 'Team members will periodically be affected with vomit-inducing sickness. Combatants struck by vomit are debilitated and inflicted with conditions.',
-    de: '',
+    de: 'Teammitglieder werden in regelmäßigen Abständen von einer Krankheit betroffen, die Brechreiz verursacht.',
   },
 };
