cc8d2a1bea9296082f164b9bdada93f570a27a12
Marcus <marcustyphoon@gmail.com>
2024-10-30 00:31:58 -0700

    replace typescript-is with typia

--- gw2-ui/src/gw2api/typeguards/guards.ts
+++ gw2-ui/src/gw2api/typeguards/guards.ts
@@ -1,13 +1,14 @@
-import { createAssertType } from 'typescript-is';
+import { createAssertEquals } from 'typia';
 
 import type GW2ApiItem from '../types/items/item';
 import type GW2ApiSkill from '../types/skills/skill';
 import type GW2ApiSpecialization from '../types/specialization/specialization';
 import type GW2ApiTrait from '../types/traits/trait';
 
-export const isGW2ApiItem = createAssertType<GW2ApiItem>();
-export const isGW2ApiSkill = createAssertType<GW2ApiSkill>();
-export const isGW2ApiSpecialization = createAssertType<GW2ApiSpecialization>();
-export const isGW2ApiTrait = createAssertType<GW2ApiTrait>();
+export const isGW2ApiItem = createAssertEquals<GW2ApiItem>();
+export const isGW2ApiSkill = createAssertEquals<GW2ApiSkill>();
+export const isGW2ApiSpecialization =
+  createAssertEquals<GW2ApiSpecialization>();
+export const isGW2ApiTrait = createAssertEquals<GW2ApiTrait>();
 
 export * from '../overrides/index';
